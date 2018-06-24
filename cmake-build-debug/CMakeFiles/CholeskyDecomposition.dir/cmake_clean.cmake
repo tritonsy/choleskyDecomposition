cd64b769402480d6f6e6b93ff4cbe01a6b8e06d5
Alex <tritonsy@gmail.com>
2018-06-24 12:40:43 +0400

    Hello!

---
+++ cmake-build-debug/CMakeFiles/CholeskyDecomposition.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CholeskyDecomposition.dir/main.cpp.obj"
+  "CholeskyDecomposition.pdb"
+  "CholeskyDecomposition.exe"
+  "CholeskyDecomposition.exe.manifest"
+  "libCholeskyDecomposition.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CholeskyDecomposition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
